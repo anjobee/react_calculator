8af7279ed40defea70b9c6276b4177a03f4e1bda
Anjo Bihis <angelo.bihis@maroonstudios.com>
2023-03-01 00:20:09 +0800

    chore: change function name

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,13 @@
 function Button (props) {
   const { height = '80', width = '80', bgColor = 'c8c8c8', value } = props
 
-  function submitForm(event) {
+  function sendButtonValue(event) {
     event.preventDefault();
     props.onClick(value);
   }
 
   return (
-    <button className="btn border" style={{height: `${height}px`, width: `${width}px`, backgroundColor: `#${bgColor}`, borderRadius: '0px'}} onClick={submitForm}>
+    <button className="btn border" style={{height: `${height}px`, width: `${width}px`, backgroundColor: `#${bgColor}`, borderRadius: '0px'}} onClick={sendButtonValue}>
       {props.value}
     </button>
   );
